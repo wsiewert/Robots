23c55415aec5fda224cc51656d1cae72c5920e01
wsiewert <wsiewert94@gmail.com>
2017-06-28 16:17:14 -0500

    Added Generic Robots Class

---
+++ Robots/Robots/Robot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq; 
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Robots
+{
+    class Robot
+    {
+        string material;
+        string powerType;
+        string color;
+        bool powerOn;
+        bool isSelfAware;
+
+        public Robot()
+        {
+
+        }
+    }
+}
